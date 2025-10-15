ea4caa3715daeafaca7f770c6fc29f5dfce39172
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-15 13:53:12 +0200

    variables

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -1,3 +1,9 @@
 """
 Le compte est bon.
 """
+import random
+
+nombre = random.randint(101, 999)
+reparties = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
+           6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
+           25, 50, 75, 100]
