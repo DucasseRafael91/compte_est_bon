7deb134ab279f4fa7c528041f441e6e8a1141159
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-15 14:05:27 +0200

    valeurs aleatoires

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -1,9 +1,13 @@
-"""
-Le compte est bon.
-"""
 import random
 
 nombre = random.randint(101, 999)
+
 reparties = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
-           6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
-           25, 50, 75, 100]
+             6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
+             25, 50, 75, 100]
+
+# Tirer 6 nombres au hasard depuis la liste (respecte les doublons disponibles)
+tirage = random.sample(reparties, 6)
+
+print("Nombre cible :", nombre)
+print("Nombres tirés :", tirage)
