e3495c500ecdb11bf9894ef8f46a386435c9ebb0
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-16 08:33:55 +0200

    Fonctions afficher_nombres + appliquer_operation

--- compte_est_bon.py
+++ compte_est_bon.py
@@ -1,13 +1,38 @@
 import random
 
-nombre = random.randint(101, 999)
 
 reparties = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5,
              6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
              25, 50, 75, 100]
 
-# Tirer 6 nombres au hasard depuis la liste (respecte les doublons disponibles)
-tirage = random.sample(reparties, 6)
+# Tirage
+cible = random.randint(101, 999)
+plaques = random.sample(reparties, 6)
 
-print("Nombre cible :", nombre)
-print("Nombres tirés :", tirage)
+# Liste des nombres disponibles : au départ, ce sont les 6 plaques
+nombres_disponibles = plaques.copy()
+
+print("Nombre à atteindre :", cible)
+
+def afficher_nombres():
+    print("🔢 Nombres disponibles :", nombres_disponibles)
+
+def appliquer_operation(op, a, b):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        if b != 0 and a % b == 0:
+            return a // b
+        else:
+            return None
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    afficher_nombres()
+    appliquer_operation(appliquer_operation, cible, plaques)
